db56da945f35d4d07f9c90a58d43b863b41763b2
* <nayeli.garcia@utelvt.edu.ec>
2022-03-15 08:38:49 -0500

    proyecto2

--- NayeliGuevara/suma.cpp
+++ NayeliGuevara/suma.cpp
@@ -2,17 +2,16 @@
 using  namespace std;
 int main (){
         float a,b,c;
+	cout<<"ingra los numeros"<<endl<<endl<<endl;
         cin>>a>>b;
 
         c=a+b;
 
 
-
-        cout<<c;
-
+        cout<<"este es el resultado de la suma"<<c;
 
 
 
 
-return 0;
-}
+
+return 0;}
